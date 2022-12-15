92b53483529271bdacfc61ae28bcb23e125049e1
ms-joker <joker728@gmail.com>
2022-12-15 22:18:35 +0300

    US22 is done by Mehmet Siddik

---
+++ src/com/cydeo/US22.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class US22 {
+    public static void main(String[] args) {
+
+        System.out.println("This is done by Mehmet Siddik.\n US22 is done here.");
+
+
+    }
+}
